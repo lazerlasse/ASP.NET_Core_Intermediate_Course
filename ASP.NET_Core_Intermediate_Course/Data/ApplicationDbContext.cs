00c82d826a891d6cd545f8171c1edf186fc4107c
Lasse Antonsen <brandmandlasse@gmail.com>
2018-11-16 15:11:35 +0100

    Add project files.

---
+++ ASP.NET_Core_Intermediate_Course/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Intermediate_Course.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
